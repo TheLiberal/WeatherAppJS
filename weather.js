ef170b9c0ff67120f9df0b1713ddf1cd88faad64
Błażej Gułaj <bgulaj@gmail.com>
2020-04-12 08:08:13 +0200

    Changed api key

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 "use strict";
 class Weather {
     constructor() {
-        this.api_key = "753817e77bafd9796a32d0ba7e32ba54";
+        this.api_key = "950b049d2e244bf9a8ccc73be60ff29d";
     }
 
     async getWeather(searchCity) {
